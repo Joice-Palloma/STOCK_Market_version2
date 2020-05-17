98739e19d0ba0cf694463a2c5fb13c0cc0e0c2dc
Joice-Palloma <joicepgm@gmail.com>
2020-05-17 13:17:15 +0100

    companyObserver created

--- STOCK_Market_Version2/src/Company_Observer.java
+++ STOCK_Market_Version2/src/Company_Observer.java
@@ -1,4 +1,16 @@
-
 public interface Company_Observer {
-
+	public void setPrice(double price);
+	
+	 public void setNumOfShares(int num);
+	 
+	  public void updateShares(int change); 
+	
+	  public int getSharesSold();
+	  
+	  public double getSharePrice();
+	  
+	  public int getSharesRemaining();
+	  
+	  public long getId();
+	  
 }
