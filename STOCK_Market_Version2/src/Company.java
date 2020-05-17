4e951112ed471740d6d450e1a7bf1c35221678db
Joice-Palloma <joicepgm@gmail.com>
2020-05-17 13:19:49 +0100

    company all the meths created

--- STOCK_Market_Version2/src/Company.java
+++ STOCK_Market_Version2/src/Company.java
@@ -1,4 +1,65 @@
 
-public class Company {
+public class Company implements Company_Observer {
+
+	private long id;
+
+    private int numOfShares;
+
+    private double sharePrice;
+
+    private int sharesSold;
+
+
+    public Company(int id) {
+
+        this.id = id;
+    }
+	
+	
+	@Override
+	public void setPrice(double price) {
+		// TODO Auto-generated method stub
+	this.sharePrice = price;	
+	}
+
+	@Override
+	public void setNumOfShares(int num) {
+		// TODO Auto-generated method stub
+		this.numOfShares = num;
+		
+	}
+
+	@Override
+	public void updateShares(int change) {
+		// TODO Auto-generated method stub
+		
+		this.numOfShares -= change;
+        this.sharesSold += change;
+		
+	}
+
+	@Override
+	public int getSharesSold() {
+		// TODO Auto-generated method stub
+		return sharesSold;
+	}
+
+	@Override
+	public double getSharePrice() {
+		// TODO Auto-generated method stub
+		return sharePrice;
+	}
+
+	@Override
+	public int getSharesRemaining() {
+		// TODO Auto-generated method stub
+		return numOfShares;
+	}
+
+	@Override
+	public long getId() {
+		// TODO Auto-generated method stub
+		return id;
+	}
 
 }
