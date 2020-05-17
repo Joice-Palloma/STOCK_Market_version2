325c7fc7459c2ca415886f22cdb4c344410bb77d
Joice-Palloma <joicepgm@gmail.com>
2020-05-17 13:10:48 +0100

    investor interface created

--- STOCK_Market_Version2/src/InvestorCollectionInterface.java
+++ STOCK_Market_Version2/src/InvestorCollectionInterface.java
@@ -1,4 +1,15 @@
+import java.util.List;
 
 public interface InvestorCollectionInterface {
+	
+	 public void addInvestor(Investor i);
 
+	    public void removeInvestor(Investor i);
+
+	    public InvestorIteratorInterface iterator();
+
+	    public Investor getInvestorWithId(int id);
+
+	    public List<Investor> getInvestorList();
+	
 }
