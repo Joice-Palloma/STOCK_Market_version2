4f57b68bddacfb5aeb2cb996a993b79d96c93b72
Joice-Palloma <joicepgm@gmail.com>
2020-05-17 13:23:13 +0100

    iterator class (investor)

--- STOCK_Market_Version2/src/InvestorIterator.java
+++ STOCK_Market_Version2/src/InvestorIterator.java
@@ -1,4 +1,34 @@
 
-public class InvestorIterator {
+import java.util.List;
+
+public class InvestorIterator implements InvestorIteratorInterface{
+
+    List<Investor> investorList;
+    int position;
+
+    public InvestorIterator(List<Investor> investors){
+        this.investorList = investors;
+        this.position = 0;
+    }
+
+    @Override
+    public boolean hasNext() {
+
+        Investor investor = null;
+
+        while(position < investorList.size()){
+            investor = investorList.get(position);
+            position++ ;
+        }
+
+        return !(investor == null);
+    }
+
+    @Override
+    public Investor next() {
+        Investor investor = investorList.get(position);
+        position++;
+        return investor;
+    }
 
 }
