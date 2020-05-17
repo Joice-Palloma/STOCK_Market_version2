c5f8600a3a3299cbbb9641643f9138ff56c774f0
Joice-Palloma <joicepgm@gmail.com>
2020-05-17 13:08:38 +0100

    investor created

--- STOCK_Market_Version2/src/Investor.java
+++ STOCK_Market_Version2/src/Investor.java
@@ -1,4 +1,29 @@
 
 public class Investor {
+	private long id;
 
+    private double budget;
+
+//    HashMap<Integer, Integer> companyStockMap;
+
+    public Investor(int id) {
+        this.id = id;
+  //      companyStockMap = new HashMap<>();
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public double getBudget() {
+        return budget;
+    }
+
+    public void setBudget(double budget) {
+        this.budget = budget;
+    }
 }
