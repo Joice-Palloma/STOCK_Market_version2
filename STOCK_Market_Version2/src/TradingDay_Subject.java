5af6d13fae8b6e1f62eb614e3278c7a8bfb43f5b
Joice-Palloma <joicepgm@gmail.com>
2020-05-17 13:53:30 +0100

    trading day subject fixed

--- STOCK_Market_Version2/src/TradingDay_Subject.java
+++ STOCK_Market_Version2/src/TradingDay_Subject.java
@@ -1,39 +1,9 @@
+public interface TradingDay_Subject {
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
+    public void register(Company_Observer company);
 
-public class TradingDay implements TradingDay_Subject{
-
-    List<Company_Observer> companies;
-
-    private Date date;
-
-    public TradingDay(){
-        this.companies = new ArrayList<>();
-    }
+    public void unregister(Company_Observer company);
 
 
-    @Override
-    public void register(Company_Observer company) {
-        if(company == null){
-            throw new NullPointerException("Null Company cannot be added");
-        }
-
-        if(!companies.contains(company)){
-            companies.add(company);
-        }
-    }
-
-    @Override
-    public void unregister(Company_Observer company) {
-
-        companies.remove(company);
-
-    }
-
-    @Override
-    public void notifyObservers() {
-
-    }
+    public void notifyObservers();
 }
